723de9e6412d869f4297ae1790b9f03484cb648c
supersonicwisd1 <supersonicwisdom@gmail.com>
2023-09-10 08:25:33 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Query
+from datetime import datetime, timedelta
+
+app = FastAPI()
+
+@app.get("/get_info")
+async def get_info(
+    slack_name: str = Query(..., description="Slack name"),
+    track: str = Query(..., description="Track"),
+):
+    # Get the current day of the week
+    current_day = datetime.utcnow().strftime('%A')
+
+    # Calculate UTC time with validation of +/-2 hours
+    utc_time = datetime.utcnow() + timedelta(hours=2)
+    utc_time_str = utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Define the GitHub URLs
+    github_file_url = f"https://github.com/{slack_name}/repo/blob/main/file_name.ext"
+    github_repo_url = f"https://github.com/{slack_name}/repo"
+
+    # Create the response JSON
+    response = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time_str,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return response
